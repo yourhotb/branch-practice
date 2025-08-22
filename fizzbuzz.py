d498c481aab3a1d6b1d029dcbd8c74d84c31b82d
yourhotb <yourhotb@gmail.com>
2025-08-22 10:47:41 +0900

    test: conflict test

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,5 @@
-for i in range(1, 16+1):
-    if i % 3 == 0:
-        print(’fizz’)
+for j in range(1, 17+1):
+    if j % 3 == 0:
+        print('fizz')
     else:
-        print(i)
+        print(j)
