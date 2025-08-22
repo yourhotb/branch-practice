c31633a455dd9bc5378adeb49454d90fcad3097a
yourhotb <yourhotb@gmail.com>
2025-08-22 14:26:55 +0900

    refactor: Do fizzbuzz with 1 Conditional Statement

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,5 @@
-for j in range(1, 16+1):
-    if j % 15 ==0:
-        print('fizzbuzz')
-    elif j % 3 == 0:
-        print('fizz')
-    elif j % 5 == 0:
-        print('buzz')
+for j in range(1, 15+1):
+    if j % 15 ==0 or 5 j % 5 ==0:
+        print('fizz' * (j % 3 == 0) + 'buzz' * (j % 5 == 0))
     else:
         print(j)
